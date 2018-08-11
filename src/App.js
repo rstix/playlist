83723c2d01e82f5e5138fa63c41d0a17d4bc5f21
rendlik <rsonroad@gmail.com>
2018-08-11 07:51:19 -0700

    populated with fake server data

--- src/App.js
+++ src/App.js
@@ -6,11 +6,51 @@ let defaultStyle = {
   color: '#444'
 }
 
-class Agregate extends Component {
+let fakeServerData = {
+  user: {
+    name: 'David',
+    playlists: [
+      {
+        name: 'My favourite',
+        songs: [{name:'Beat it', duration: 1234}, {name: 'Nina', duration: 7524}, {name: 'Lucie', duration: 5231}]
+      },
+      {
+        name: 'Morning run',
+        songs: [{name:'Beat it', duration: 1234}, {name: 'Nina', duration: 7524}, {name: 'Lucie', duration: 5231}]
+      },
+      {
+        name: 'Chill',
+        songs: [{name:'Beat it', duration: 1234}, {name: 'Nina', duration: 7524}, {name: 'Lucie', duration: 5231}]
+      },
+      {
+        name: 'Trip',
+        songs: [{name:'Beat it', duration: 1234}, {name: 'Nina', duration: 7524}, {name: 'Lucie', duration: 5231}]
+      }
+    ]
+  }
+}
+
+class PLaylistCounter extends Component {
   render(){
     return (
       <div style={{...defaultStyle, display: 'inline-block', width: '40%'}}>
-        <h2>Agregate component</h2>
+        <h2> {this.props.playlists.length} playlists</h2>
+      </div>
+    )
+  }
+}
+
+class HourCounter extends Component {
+  render(){
+    let allSongs = this.props.playlists.reduce((songs,playlist)=>{
+      return songs.concat(playlist.songs)
+    },[])
+    let totalDuration = allSongs.reduce((duration,song) =>{
+      return duration + song.duration
+    },0)
+    return (
+      <div style={{...defaultStyle, display: 'inline-block', width: '40%'}}>
+        <h2> {Math.round(totalDuration/3600)} hours</h2>
       </div>
     )
   }
@@ -43,17 +83,32 @@ class Playlist extends Component {
 }
 
 class App extends Component {
+  constructor(){
+    super()
+    this.state = {serverData: {}}
+  }
+  componentDidMount(){
+    setTimeout(()=>{
+      this.setState({
+        serverData: fakeServerData
+      })
+    },2000)
+  }
   render() {
-    let name = 'Tom'
     return (
       <div className="App">
-        <h1 style={defaultStyle}> Title {name} </h1>
-        <Agregate/> 
-        <Agregate/>
-        <Filter/>
-        <Playlist/>
-        <Playlist/>
-        <Playlist/>
+        {this.state.serverData.user ?
+        <div>
+          <h1 style={defaultStyle}>
+            {this.state.serverData.user.name}'s playlist 
+          </h1>
+          <PLaylistCounter playlists={this.state.serverData.user.playlists}/> 
+          <HourCounter playlists={this.state.serverData.user.playlists}/>
+          <Filter/>
+          <Playlist/>
+          <Playlist/>
+          <Playlist/>
+        </div> : <h1 style={defaultStyle}> Loading... </h1>}
       </div>
     );
   }
