c60dbe2d912fb30a4f4c6b964e6fd7a8e536ec47
rendlik <rsonroad@gmail.com>
2018-08-14 22:06:07 -0700

    fetching data from spotify api

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 // import logo from './logo.svg';
 import './App.css';
-import queryString from 'query-string';
+import queryString from 'query-string-es5';
 
 let defaultStyle = {
   color: '#444',
